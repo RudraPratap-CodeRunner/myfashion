e76b108251cf74107c9c8100a8a275bad979dfd2
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-08-11 09:16:39 +0530

    image storage added witk multer

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,34 @@ app.use(express.json());
 app.use(cors());
 
 // connect with database 
-mongoose.connect('connection string')
+// mongoose.connect('connection string')
 
 
 app.get('/',(req,res)=>{
     res.send("Express app is running")
 })
 
+//Image Storage Engine 
+const storage = multer.diskStorage({
+  destination: './upload/images',
+  filename: (req, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `/images/${req.file.filename}`
+  })
+})
+
+
+// Route for Images folder
+app.use('/images', express.static('upload/images'));
+
 // Starting Express Server
 app.listen(port, (error) => {
-  if (!error) console.log("Server Running on port " + port);
+  if (!error) console.log(`server running on http://localhost:${port}`);
   else console.log("Error : ", error);
 });
