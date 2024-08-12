883ac8404b89b9491d933ecba59986cfe4d858d0
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-08-12 16:06:39 +0530

    user Schema and endpoint fot signup addded

--- backend/index.js
+++ backend/index.js
@@ -75,6 +75,52 @@ app.post("/addproduct", async (req, res) => {
 // Route for Images folder
 app.use('/images', express.static('upload/images'));
 
+// endpoint for getting all products data
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products");
+  res.send(products);
+});
+
+// Schema for creating user model
+const Users = mongoose.model("Users", {
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  cartData: { type: Object },
+  date: { type: Date, default: Date.now() },
+});
+
+//Create an endpoint at ip/auth for regestring the user & sending auth-token
+app.post('/signup', async (req, res) => {
+  console.log("Sign Up");
+  let success = false;
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({ success: success, errors: "existing user found with this email" });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+
+  const token = jwt.sign(data, 'secret_ecom');
+  success = true;
+  res.json({ success, token })
+})
+
 // Starting Express Server
 app.listen(port, (error) => {
   if (!error) console.log(`server running on http://localhost:${port}`);
